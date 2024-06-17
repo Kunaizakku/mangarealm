7cace20eae264a0f1b421fd94ea182408c903c13
carlos ramirez <carlosraamirez13@gmail.com>
2024-06-17 08:25:45 -0700

    navbar y css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hola tonotos</title>
+    <link rel="stylesheet" href="app.css">
+    <style>    
+    </style>
 </head>
 <body>
+    <div class="navbar">
+        <div class="logo">
+            M A N G A R E A L M
+        </div>
+        <div class="menu">
+            <a href="#home">Inicio</a>
+            <a href="#about">Ejemplo 1</a>
+            <a href="#services">Ejemplo 2</a>
+            <a href="#contact">Ejemplo 3</a>
+            <a href="{{ route('login') }}">Cerrar Sesión</a>
+        </div>
+    </div>
+
     <h1>Hola tonotos</h1>
-    <a href="{{ route('login') }}">hola</a>
+    <p>Bienvenido a mi sitio web.</p>
 </body>
 </html>
