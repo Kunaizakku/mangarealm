fe922ffc3f811309fd9e32a436a72d3dd4a01c19
Brayan Isaac Toledo Gomez <brayantoledo@solemti.com>
2024-07-01 12:32:12 -0700

    mejoras

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\registroController;
 use App\Http\Controllers\CategoriaController;
-
+use App\Http\Controllers\MangaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,13 +11,15 @@ Route::get('/', function () {
 
 Route::post('/registroUsuario', [registroController::class, 'insertar'])->name('usuario.insertar');
 Route::post('/registroCategoria', [CategoriaController::class, 'insertar'])->name('cat.insertar');
-Route::get('/listadoCat', [Categoriacontroller::class,'mostrar'])->name('cat.mostrar');
+Route::post('/registroManga', [MangaController::class, 'insertar'])->name('man.insertar');
+Route::get('/listadoCat', [CategoriaController::class,'mostrar'])->name('cat.mostrar');
 Route::get('/categorias', [Categoriacontroller::class,'ver'])->name('cat.listas');
-
 
 Route::get('/login', function () {return view('login');})->name('login');
 Route::get('/formularios', function () {return view('formularios');})->name('formularios');
 Route::get('/form_cat', function () {return view('form_cat');})->name('form_cat');
 Route::get('/mangas', function () {return view('manga');})->name('mangas');
 Route::get('/categoria de mangas', function () {return view('cat_manga');})->name('cat.manga');
+Route::get('/listadoMan', [MangaController::class,'mostrar'])->name('man.mostrar');
+
 // Route::get('/categoria', function () {return view('categoria');})->name('cat');
